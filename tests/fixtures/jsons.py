517fe3cf0bba083e3368c3965c38f4470caed073
Cesar E Garza <cesar.garza.826@gmail.com>
2022-10-16 00:14:45 -0500

    started writing tests

---
+++ tests/fixtures/jsons.py
@@ -0,0 +1,40 @@
+import json
+from pathlib import Path
+
+import pytest
+
+from squidalytics.schemas.battle import battleSchema
+
+input_path = Path(__file__).parent.parent / "input_files"
+
+
+def json_path(
+    test_json: dict, path: tuple[str | int, ...]
+) -> dict | list[dict]:
+    """Get a value from a nested dictionary by a path of keys. Replaces having
+    to the messy syntax of `test_json["key1"]["key2"]`.
+
+    Args:
+        test_json (dict): The dictionary to get the value from.
+        path (tuple[str | int, ...]): The path of keys to get the value from.
+
+    Returns:
+        dict | list[dict]: The value at the end of the path.
+    """
+    out: dict | list[dict] = test_json
+    for key in path:
+        out = out[key]
+    return out
+
+
+@pytest.fixture
+def anarchy_full_json() -> dict:
+    anarchy_path = str(input_path / "test_anarchy.json")
+    with open(anarchy_path, "r") as f:
+        anarchy = json.load(f)
+    return anarchy
+
+
+@pytest.fixture
+def anarchy_battle_schema(anarchy_full_json: dict) -> battleSchema:
+    return battleSchema(anarchy_full_json)
---
+++ tests/test_schema.py
@@ -0,0 +1,52 @@
+from typing import Any
+import pytest
+
+from squidalytics.schemas.base import JSONDataClass
+from squidalytics.schemas.battle import (
+    awardsSchema,
+    backgroundSchema,
+    badgeSchema,
+    badgesSchema,
+    bankaraMatchSchema,
+    battleDataSchema,
+    battleNodeSchema,
+    battleSchema,
+    brandSchema,
+    gearPowerSchema,
+    gearSchema,
+    nameplateSchema,
+    playerFullSchema,
+    playerSchema,
+    resultSchema,
+    teamSchema,
+    usualGearPowerSchema,
+    vsHistoryDetailSchema,
+)
+from tests.fixtures.jsons import json_path
+
+base_path = (0, "data", "vsHistoryDetail")
+
+
+class TestAnarchySchema:
+    @pytest.mark.parametrize(
+        "path, expected",
+        [
+            (("vsRule", "name"), "Clam Blitz"),
+            (("vsRule", "rule"), "CLAM"),
+            (("vsMode", "mode"), "BANKARA"),
+            (("player", "name"), "Joy"),
+            (("player", "nameplate", "background", "textColor", "a"), 1.0),
+        ],
+    )
+    def test_getattr(
+        self,
+        anarchy_full_json: dict,
+        anarchy_battle_schema: battleSchema,
+        path: tuple,
+        expected: Any,
+    ) -> None:
+        """Test that the schema can get correct attributes from the JSON."""
+        true_path = base_path + path
+        test_json = json_path(anarchy_full_json, true_path)
+        assert anarchy_battle_schema[true_path] == test_json
+        assert anarchy_battle_schema[true_path] == expected
