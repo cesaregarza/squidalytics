d0b8f8e8388527976b5ebf722063ee1ff8e6ec0f
Cesar E Garza <cesar@cegarza.com>
2023-05-14 03:09:41 -0500

    finished scraper

--- squidalytics/scripts/build_scraper.py
+++ squidalytics/scripts/build_scraper.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime as dt
 from typing import TypeAlias
 
@@ -5,11 +6,15 @@ import numpy as np
 import numpy.typing as npt
 import pandas as pd
 import sqlalchemy as sa
+from dotenv import load_dotenv
+from sqlalchemy.engine import URL
 
 from squidalytics.analytics.build_consensus.main import generate_all_abilities
 from squidalytics.analytics.build_consensus.scrape_sendou import (
     scrape_sendou_builds,
 )
+
+load_dotenv()
 
 BuildType: TypeAlias = dict[str, str | list[dict[str, str | list[dict]]]]
 
@@ -47,7 +52,7 @@ def sqlify_build(build: dict) -> dict:
     out = {}
     for mode in MODES:
         out[mode] = mode in build["modes"]
-    out["author"] = build["author"]
+    out["author"] = build["author"].replace("\x00", "")
     out["plus"] = build["plus"]
     out["top_500"] = build["top_500"]
     out["hash"] = build["hash"]
@@ -55,11 +60,32 @@ def sqlify_build(build: dict) -> dict:
     return out
 
 
-def get_existing_hashes(conn: sa.engine.Connection) -> list[str]:
-    query = sa.select([sa.column("hash")]).select_from(sa.table("builds"))
-    return [row["hash"] for row in conn.execute(query)]
+def get_existing_hashes(engine: sa.engine.Engine) -> list[str]:
+    query = "SELECT hash FROM builds"
+    return pd.read_sql(query, engine)["hash"].tolist()
 
 
-def scrape(conn: sa.engine.Connection) -> None:
-    existing_hashes = get_existing_hashes(conn)
-    scraped_builds = scrape_sendou_builds(1000, hash_list=existing_hashes)
+def scrape(engine: sa.engine.Engine) -> None:
+    existing_hashes = get_existing_hashes(engine)
+    scraped_builds = scrape_sendou_builds(48, hash_list=existing_hashes)
+    builds_df = sqlify_scraped_builds(scraped_builds)
+    if len(builds_df) == 0:
+        return
+    builds_df.to_sql("builds", engine, if_exists="append", index=False)
+
+
+def main() -> None:
+    engine_data = {
+        "username": os.getenv("POSTGRES_USER"),
+        "password": os.getenv("POSTGRES_PASSWORD"),
+        "host": os.getenv("POSTGRES_HOST"),
+        "port": os.getenv("POSTGRES_PORT"),
+        "database": os.getenv("POSTGRES_DB"),
+    }
+    url_object = URL.create("postgresql+psycopg2", **engine_data)
+    engine = sa.create_engine(url_object)
+    scrape(engine)
+
+
+if __name__ == "__main__":
+    main()
